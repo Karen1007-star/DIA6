c8b7e7b27a46ee622b08a740c52ae9028859c562
Selene <karen_08_121@hotmail.com>
2026-03-23 18:17:45 -0500

    ya vamos con GET

--- appNODENJS.js
+++ appNODENJS.js
@@ -102,36 +102,109 @@
 //     console.log(`Servidor corriendo`);
 // })
 
-// 🧪 MINI EXAMEN
+// // 🧪 MINI EXAMEN
 
-const http = require("http")
-const servidor = http.createServer((req,res)=>{
-    const ruta = new URL (req.url,"http://localhost:3000")
-    if(ruta.pathname === "/"){
-        res.end("Bienvenido al sistema");
-    }else if(ruta.pathname==="/usuario"){
-        const nombre = ruta.searchParams.get('nombre')
-        const edad = ruta.searchParams.get('edad')
-        if (!nombre || !edad) {
-            res.end("Faltan datos");
-            return;
+// const http = require("http")
+// const servidor = http.createServer((req,res)=>{
+//     const ruta = new URL (req.url,"http://localhost:3000")
+//     if(ruta.pathname === "/"){
+//         res.end("Bienvenido al sistema");
+//     }else if(ruta.pathname==="/usuario"){
+//         const nombre = ruta.searchParams.get('nombre')
+//         const edad = ruta.searchParams.get('edad')
+//         if (!nombre || !edad) {
+//             res.end("Faltan datos");
+//             return;
+//         }
+//         res.end(`Usuario: ${nombre} - Edad: ${edad}`)
+//     }else if(ruta.pathname==="/producto"){
+//         const item = ruta.searchParams.get('item')
+//         const precio = ruta.searchParams.get('precio')
+//         res.end(`El ${item} cuesta ${precio} soles`)
+//     }
+// })
+
+// servidor.listen(3000,()=>{
+//     console.log("ESCUCHANDO");
+// })
+
+/////////////////////////////////////////// express
+
+// const expre = require ("express")
+// const app = expre()
+
+// app.get("/", (req,res)=>{
+//     res.send("Bienvenido al sistema")
+// });
+// app.get("/hola",(req,res)=>{
+//     res.send("Hola Karen")
+// })
+// app.get("/usuarios",(req,res)=>{
+//     const nombre = req.query.nombre;
+//     const edad = req.query.edad;
+//     res.send(`Usuario: ${nombre} - edad:${edad}`)
+// })
+// app.get("/producto",(req,res)=>{
+//     const item = req.query.item;
+//     const precio = req.query.precio;
+//     res.send(`${item} cuesta S/.${precio}`)
+// })
+
+// app.listen(3000,()=>{
+//     console.log("Servidor en http://localhost:3000");
+// })
+
+//////////////////////////////////////////// EXPRESS
+// const expresito = require("express")
+// const ruta = expresito()
+
+// ruta.get("/",(req,res)=>{
+//     res.send("BienVenIDa")
+// })
+
+// ruta.get("/usuario/:nombre/:edad",(req,res)=>{
+//     const {nombre,edad}=req.params
+//     if(!nombre || !edad) return res.send("NO hay datos");
+//     res.send(`Mi nombre es ${nombre} y mi edad ${edad} años`)
+// })
+// ruta.get("/producto",(req,res)=>{
+//     const {item,precio}=req.query
+//     if(!item || !precio) return res.send("NO hay datos");
+//     res.send(`${item} cuesta ${precio}`)
+// })
+// ruta.use((req, res) => {
+//     res.status(404).send("Ruta no encontrada");
+// });
+
+// ruta.listen(3000,()=>{
+//     console.log("Escuchando 3000000 ");
+// })
+
+
+///////////// 🧪 Recomendación antes de seguir - JSON
+const expresito = require("express")
+const fs = require("fs");
+
+const rutita = expresito();
+rutita.use(expresito.json()) // ESTO ES middleware
+
+rutita.get("/",(req,res)=>{
+    res.send("bienvidos")
+})
+
+rutita.get("/usuarios", (req,res)=>{
+    fs.readFile("./usuarios.json", "utf-8",(error,data)=>{
+        if(error) {
+            return res.status(500).json({error: "Error leyendo archivo"})
         }
-        res.end(`Usuario: ${nombre} - Edad: ${edad}`)
-    }else if(ruta.pathname==="/producto"){
-        const item = ruta.searchParams.get('item')
-        const precio = ruta.searchParams.get('precio')
-        res.end(`El ${item} cuesta ${precio} soles`)
-    }
+        const usuarios = JSON.parse(data)
+        res.status(200).json(usuarios)
+    })
 })
 
-servidor.listen(3000,()=>{
-    console.log("ESCUCHANDO");
+rutita.listen(3000,()=>{
+    console.log("escuchando");
 })
-
-
-
-
-
 
 
 
